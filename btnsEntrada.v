9c56b56502f3cf480130f7b15801a56c7f76e72d
Silas-Silva <silllas22@gmail.com>
2022-04-04 13:15:55 -0300

    changes

--- btnsEntrada.v
+++ btnsEntrada.v
@@ -9,9 +9,9 @@ module btnsEntrada ( A, B, C, N);
 	xor Xor0(T1, A, B);
 	not Not0(T2, C);
 	and And0 (N[0], T1, T2);
-	
 	xor Xor1(T3, A, C);
 	not Not1(T4, B);
-	and And1(N[1], T3, t4);
-			
+	and And1 (N[1], T3, T4);
+	
+	
 endmodule 
