385013cd3ba20e7cd0d5c1a184a5f19631f2aa4d
Silas-Silva <silllas22@gmail.com>
2022-04-04 11:41:20 -0300

    Changes

--- Entradas_soma.v
+++ Entradas_soma.v
@@ -1,12 +1,13 @@
-module Entradas_soma ( A, B, C, Chave, N1, Nbtoes, S);
+module Entradas_soma ( A, B, C, Chave, N1, SaidaBtns, S, Cout);
 	input A, B, C, Chave;
 	input [6:0] N1;
-	input [1:0] Nbtoes;
+	output [1:0] SaidaBtns;
 	output [6:0] S;
+	output Cout;
 	
 		
-	btnsEntrada entradas (.A(A), .B(B), .C(C), .N(Nbtoes));
-	somador7bts soma (.A(N1), .B(Nbtoes), .Cin(Chave), .S(S), .Cout(Cout));
+	btnsEntrada entradas (.A(A), .B(B), .C(C), .N(SaidaBtns));
+	somador7bts r1 (.A(N1), .B(SaidaBtns), .Cin(Chave), .S(S), .Cout(Cout));
 	
 	
 	
