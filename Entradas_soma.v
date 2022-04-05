32e9fb1adcba373f6df7206ea5094a2ad4b7c0c5
Silas-Silva <silllas22@gmail.com>
2022-04-05 11:41:01 -0300

    buzzer para error

--- Entradas_soma.v
+++ Entradas_soma.v
@@ -16,36 +16,25 @@ module Entradas_soma ( A, B, C, ChaveNegativaPositiva, MudarTime, N1, SaidaBtns,
 	//Saida com a resposta 1 para numero valido 0 para invalido
 	
 	//Verificar se o valor de 'N1' é maior que o de 'SaidaBtns'
-		
-//	
-//	always @* begin 
-//		case (ChaveNegativaPositiva)
-//			1'b1 : begin 
-//				case (valido)
-//					1'b0 : begin 
-//						//Se não 'Ligar led ou busina'
-//						buzzer = 1'b1;
-//					end 
-//					1'b1 : begin 
-//						//Se for verdade 'Soma' e mostra
-//						somador_subtrator7bts resultado0 (.A(N1), .B(SaidaBtns), .Cin(ChaveNegativaPositiva), .S(S), .Cout(Cout));
-//						//Decodificador para mostrar no display
-//						decodificadorDisplay7Segmentos display0 (.pontos(S), .timeBasquete(MudarTime), .display1Time0(time1d1),.display2Time0(time1d2), .display1Time1(time2d1), .display2Time1(time2d2));
-//						buzzer = 1'b0;
-//					end
-//					
-//					default: buzzer = 1'b0;  
-//				endcase
-//			end
-//			1'b0 : begin
-//				//Se for verdade 'Soma' e mostra
-//				somador_subtrator7bts resultado1 (.A(N1), .B(SaidaBtns), .Cin(ChaveNegativaPositiva), .S(S), .Cout(Cout));
-//				//Decodificador para mostrar no display
-//				decodificadorDisplay7Segmentos display1 (.pontos(S), .timeBasquete(MudarTime), .display1Time0(time1d1),.display2Time0(time1d2), .display1Time1(time2d1), .display2Time1(time2d2));
-//			end
-//		endcase 
-//	end 
-//	
+	always @* begin 
+		case (ChaveNegativaPositiva)
+			1'b1 : begin 
+				case (valido)
+					1'b0 : begin 
+						//Se não 'Ligar led ou busina'
+						buzzer = 1'b1;
+					end	
+					default: buzzer = 1'b0;  
+				endcase
+			end
+		endcase 
+	end 
+	
+	//Se for verdade 'Soma' e mostra
+	somador_subtrator7bts resultado (.A(N1), .B(SaidaBtns), .Cin(ChaveNegativaPositiva), .S(S), .Cout(Cout));
+	//Decodificador para mostrar no display
+	decodificadorDisplay7Segmentos display (.pontos(S), .timeBasquete(MudarTime), .display1Time0(time1d1),.display2Time0(time1d2), .display1Time1(time2d1), .display2Time1(time2d2));	
+	
 
 	
 endmodule 
